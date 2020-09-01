d3b0f66bfaed6c902cefde8d6571aa02dd19a168
Farad Niftaliyev <fngo@MacBook-Pro.local>
2020-09-01 09:40:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import data from "./data.json";
+// testing reopo
+function App() {
+  const [state, setState] = useState({
+    products: data.products,
+    sleeps: 0,
+    sort: "",
+  });
+  return (
+    <div className="grid-container">
+      <header>
+        <h5>Header</h5>
+      </header>
+      <main>
+        <div className="content"></div>
+      </main>
+      <footer>All right is reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
