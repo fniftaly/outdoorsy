5d3923fdeb2e21ebc8312f7adebf5afc6d55c5ee
Farad Niftaliyev <fngo@MacBook-Pro.local>
2020-09-01 09:53:09 -0700

    hell hell

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import data from "./data.json";
+import data from "./data.json";0
 // testing reopo
 function App() {
   const [state, setState] = useState({
